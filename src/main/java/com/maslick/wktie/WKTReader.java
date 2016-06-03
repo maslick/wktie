d4f491f14a06a535dc099f6d2c6cecaa49de412f
Pavel Maslov <pavel.maslov@cloud.si>
2016-06-04 01:02:42 +0200

    added a couple of tests (Linestring), reader, writer

--- src/main/java/com/maslick/wktie/WKTReader.java
+++ src/main/java/com/maslick/wktie/WKTReader.java
@@ -4,9 +4,13 @@ import com.sinergise.geometry.Geometry;
 import com.sinergise.geometry.GeometryCollection;
 import com.sinergise.geometry.LineString;
 import com.sinergise.geometry.Point;
+import lombok.*;
+
+import java.util.ArrayList;
+
 
 public class WKTReader {
-	
+
 	/**
 	 * Transforms the input WKT-formatted String into Geometry object
 	 * POINT (30 10)
@@ -15,23 +19,68 @@ public class WKTReader {
 	public Geometry read(String wktString) {
 		//TODO: Implement this
 		Geometry ret=null;
+		String args = "";
 
-		String geoType = wktString.split(" ")[0];
-		String rest = wktString.substring(wktString.indexOf(' ')+1);
-		String args = rest.split("[\\(\\)]")[1];
-		switch (geoType) {
+		String objType = getObjectType(wktString);
+		String rest = wktString.substring(wktString.indexOf(' ') + 1);
+
+		if (!rest.equals("EMPTY")) {
+			args = getObjectArgs(wktString);
+		}
+		switch (objType) {
 			case "POINT":
-				double x = Double.parseDouble(args.split(" ")[0]);
-				double y = Double.parseDouble(args.split(" ")[1]);
-				ret = new Point(x,y);
+				if (rest.equals("EMPTY"))
+					ret = new Point();
+				else {
+					Tuple t = parseArgs(args).get(0);
+					ret = new Point(t.getX(),t.getY());
+				}
 				break;
 			case "LINESTRING":
+				if (rest.equals("EMPTY"))
+					ret = new LineString();
+				else {
+					ArrayList<Tuple> list = parseArgs(args);
+					for (int i=0; i<list.size(); i ++) {
+
+					}
+				}
 				break;
 			default:
 				ret=null;
 				break;
 		}
 		return ret;
+	}
+
+	public String getObjectType(String str) {
+		return str.split(" ")[0];
+	}
+
+	public String getObjectArgs(String str) {
+		return str.substring(str.indexOf(' ') + 1).split("[\\(\\)]")[1];
+	}
+
+	public ArrayList<Tuple> parseArgs(String str) {
+		String[] splitByComma = str.split(", ");
+		String[] splitBySpace;
+		ArrayList<Tuple> list = new ArrayList<>();
+		for (int i=0; i<splitByComma.length; i++) {
+			splitByComma[i] = splitByComma[i].trim();
+			splitBySpace = splitByComma[i].split(" ");
+			list.add(new Tuple(Double.parseDouble(splitBySpace[0]),Double.parseDouble(splitBySpace[1])));
+		}
+
+		return list;
+	}
+
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public class Tuple {
+		double x;
+		double y;
 	}
 
 }
--- test/com/maslick/wktie/WKTieTest.java
+++ test/com/maslick/wktie/WKTieTest.java
@@ -2,8 +2,12 @@ package com.maslick.wktie;
 
 
 import com.sinergise.geometry.Geometry;
+import com.sinergise.geometry.LineString;
 import com.sinergise.geometry.Point;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -24,15 +28,53 @@ public class WKTieTest {
 
     @Test
     public void testingReaderPoint() {
-        String input = "POINT (2.0 1.0)";
-        Geometry output = new Point(2,1.0);
-        assertEquals(output, (new WKTReader().read(input)));
+        String input = "POINT (2 1)";
+        Geometry expected = new Point(2,1);
+        assertEquals(expected, (new WKTReader().read(input)));
+    }
+
+    @Test
+    public void testingReaderEmptyPoint() {
+        String input = "POINT EMPTY";
+        Geometry expected = new Point();
+        assertEquals(expected, (new WKTReader().read(input)));
     }
 
     @Test
     public void testingWriterPoint() {
-        Geometry input = new Point(2,1.0);
-        String output = "PT(2.0 1.0)";
-        assertEquals(output, (new WKTWriter()).write(input));
+        Geometry input = new Point(1, 1.123);
+        String expected = "POINT (1 1.123)";
+        assertEquals(expected, (new WKTWriter()).write(input));
+    }
+
+    @Test
+    public void testingWriterEmptyPoint() {
+        Geometry input = new Point();
+        String expected = "POINT EMPTY";
+        assertEquals(expected, (new WKTWriter()).write(input));
+    }
+
+    @Test
+    public void testingWriterLine() {
+        Geometry input = new LineString(new double[]{30.123, 10, 10, 30, 40, 40});
+        String expected = "LINESTRING (30.123 10, 10 30, 40 40)";
+        assertEquals(expected, (new WKTWriter()).write(input));
+    }
+
+    @Test
+    public void testingWriterEmptyLine() {
+        Geometry input = new LineString();
+        String expected = "LINESTRING EMPTY";
+        assertEquals(expected, (new WKTWriter()).write(input));
+    }
+
+    @Test
+    public void testingTuple() {
+        WKTReader r = new WKTReader();
+        String expected = "30.123 10, 10 30, 40 40";
+        ArrayList<WKTReader.Tuple> list = r.parseArgs(expected);
+        for (int i=0; i<list.size(); i ++) {
+            System.out.println(list.get(i).getX() + " " + list.get(i).getY());
+        }
     }
 }
